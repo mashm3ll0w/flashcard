cfc8a9c005305047dee119e2b0032adf313bddf2
Charles Swaleh <charlswall@live.com>
2019-08-06 10:25:45 +0300

    add a test code block

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
   $("h3#js").click(function() {
     $("p.js-def").html("JavaScript Definition");
+
+    $("p.js-def").click(function() {
+      $(this).hide();
+    });
   });
+
   $("h3#operator").click(function() {
     $("p.op-def").html("Operator Definition");
+    $("p.op-def").hide();
   });
   $("h3#variable").click(function() {
     $("p.var-def").html("Variable Definition");
+    $("p.var-def").hide();
   });
 });
