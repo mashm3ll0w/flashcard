93c6e989b2b54117bd4acd173c2421d93cd0773a
Charles Swaleh <charlswall@live.com>
2019-09-03 13:15:23 +0300

    use forEach method to select headings and paragraphs

--- js/index.js
+++ js/index.js
@@ -1,18 +1,13 @@
+var headings = ["js", "op", "var", "var-naming", "functions", "string", "method", "argument", "parameter", "return", "alert", "jquery"];
+
 $(document).ready(function() {
-  $("h3#js").click(function() {
-    $("p.js-def").html("JavaScript Definition");
 
-    $("p.js-def").click(function() {
-      $(this).hide();
+  headings.forEach(function(heading) {
+    $("h3#" + heading).click(function() {
+      $("p." + heading + "-def").html("Definition");
     });
-  });
-
-  $("h3#operator").click(function() {
-    $("p.op-def").html("Operator Definition");
-    $("p.op-def").hide();
-  });
-  $("h3#variable").click(function() {
-    $("p.var-def").html("Variable Definition");
-    $("p.var-def").hide();
+    $("p." + heading + "-def").click(function() {
+      $(this).html("");
+    });
   });
 });
